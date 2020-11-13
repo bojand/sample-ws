b709c4bc7c801d80920508b8c2d2b1cda9f20396
Bojan <dbojan@gmail.com>
2020-11-13 12:17:03 -0400

    fix dir

--- client/index.js
+++ client/index.js
@@ -10,6 +10,12 @@ const app = express()
 
 let html = fs.readFileSync(path.join(__dirname, 'index.tmpl.html'), 'utf8')
 html = html.replace('{{location}}', process.env.WS_LOCATION || '${location.host}')
+
+const dir = path.join(__dirname, 'public')
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir)
+}
+
 fs.writeFileSync(path.join(__dirname, 'public/index.html'), html)
 
 app.use(express.static(path.join(__dirname, '/public')))
