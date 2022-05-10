96c59dc5dd88a06d870cdb21ba1151c72b1a6576
Bojan <dbojan@gmail.com>
2022-05-09 21:59:21 -0300

    simplify to a single app

---
+++ index.js
@@ -0,0 +1,65 @@
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+const { createServer } = require('http')
+const { parse } = require('url')
+const { WebSocketServer } = require('ws')
+
+const port = process.env.PORT || 8080
+
+const wsAddr = process.env.WS_HOST || '${location.host}'
+
+const app = express()
+
+let html = fs.readFileSync(path.join(__dirname, 'index.tmpl.html'), 'utf8')
+
+let protcol = process.env.NODE_ENV === 'production' ? 'wss' : 'ws'
+
+html = html.replace('{{ws_location}}', `${protcol}:${wsAddr}`)
+
+const dir = path.join(__dirname, 'public')
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir)
+}
+
+fs.writeFileSync(path.join(__dirname, 'public/index.html'), html)
+
+app.use(express.static(path.join(__dirname, '/public')))
+
+const server = createServer(app)
+const wss = new WebSocketServer({ noServer: true })
+
+wss.on('connection', function (ws, req) {
+  console.log('got connection')
+  console.log(JSON.stringify(req.headers))
+
+  const id = setInterval(function () {
+    console.log('sending message to client')
+    ws.send(JSON.stringify(process.memoryUsage()))
+  }, 1000)
+
+  console.log('started client interval')
+
+  ws.on('close', function () {
+    console.log('stopping client interval')
+    clearInterval(id)
+  })
+})
+
+server.on('upgrade', function upgrade (request, socket, head) {
+  const { pathname } = parse(request.url)
+
+  if (pathname === '/ws') {
+    wss.handleUpgrade(request, socket, head, function done (ws) {
+      wss.emit('connection', ws, request)
+    })
+  } else {
+    socket.destroy()
+  }
+})
+
+server.listen(port, function () {
+  console.log(`Listening on http://0.0.0.0:${port}`)
+})
